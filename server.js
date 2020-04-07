7bd787f5e3bc63a5041430f586fd90ccdecdf567
wdaz <haqverdi.r@gmail.com>
2020-04-07 23:05:24 +0400

    make it bet

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(history({
     verbose: true
 }));
 
-app.use('/', express.static(path.join(__dirname, 'dist/DemoTranslateApp')));
+app.use('/', express.static(path.join(__dirname, 'dist')));
 
 var port = process.env.PORT || 8080;
 app.listen(port);
